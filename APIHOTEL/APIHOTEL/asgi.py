5ec440232c005c6b37630cb7b74cc6c45b61d139
SPECTER1807 <vicmar0718@gmail.com>
2023-11-14 09:12:45 -0600

    Mi primer comentario

---
+++ APIHOTEL/APIHOTEL/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for APIHOTEL project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APIHOTEL.settings')
+
+application = get_asgi_application()
